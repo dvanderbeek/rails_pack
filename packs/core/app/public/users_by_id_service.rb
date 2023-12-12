f588de05e17aeaf03c518eec5077795d4c5728af
David Van Der Beek <earlynovrock@gmail.com>
2023-12-11 18:48:30 -0800

    move uniq call

--- packs/core/app/public/users_by_id_service.rb
+++ packs/core/app/public/users_by_id_service.rb
@@ -2,6 +2,6 @@ class UsersByIdService
   attr_reader :users
 
   def initialize(ids)
-    @users = User.where(id: ids).uniq.index_by(&:id)
+    @users = User.where(id: ids.uniq).index_by(&:id)
   end
 end
